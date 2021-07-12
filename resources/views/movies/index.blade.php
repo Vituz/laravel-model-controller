017f50886e1909f1d0fc1e25744cb55a389bc839
Vituz <vtbattaglia@gmail.com>
2021-07-12 18:57:37 +0200

    Add style to card

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -5,21 +5,32 @@
 
 @section('content')
 
-<h1>Movies</h1>
 
-@foreach($movies as $movie)
+<div class="container">
+
+    <section>
+        <h1>Movies</h1>
+
+        <div class="card_container">
+            @foreach($movies as $movie)
+
+            <div class="card">
+                <h2>{{$movie->title}}</h2>
+
+                <h3>Original title: {{$movie->original_title}}</h3>
+
+                <h4>Date: {{$movie->date}}</h4>
+
+                <h4>Vote: {{$movie->vote}}</h4>
+            </div>
+
+            @endforeach
+        </div>
+    </section>
 
 
-<div class="card">
-    <h2>{{$movie->title}}</h2>
-
-    <h3>Original title: {{$movie->original_title}}</h3>
-
-    <h4>Date: {{$movie->date}}</h4>
-
-    <h4>Vote: {{$movie->vote}}</h4>
 </div>
 
-@endforeach
+
 
 @endsection
